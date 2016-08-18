4adac4e2beb8d2cf2c6a2c1f2cbd838deb93d6fd
Kenetk <KNTK@KENETK.local>
2016-08-18 16:55:58 -0500

    Modelo ficha en app/ficha

--- apps/ficha/models.py
+++ apps/ficha/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Ficha(models.Model):
+	estado = models.CharField(max_length = 20)
+	fechaafiliacion = models.DateField(auto_now_add=True)
+	alcance = mmodels.CharField(max_length = 10)
+	paterno = mmodels.CharField(max_length = 75)
+	materno = mmodels.CharField(max_length = 75)
+	nombres = mmodels.CharField(max_length = 75)
+	estadocivil = mmodels.CharField(max_length = 1)
+	sexo = mmodels.CharField(max_length = 1, default="S")
+	lugarnacimiento = mmodels.CharField(max_length = 255)
+	direccionactual = mmodels.CharField(max_length = 255)
+	numeroactual = mmodels.IntegerField()
+	sectoractual = mmodels.CharField(max_length = 255)
+	telefonoactual = mmodels.CharField(max_length = 20)
+	correos = mmodels.TextField(blank = True, null=True)
+
+	def __str__(self):
+		return self.nombres + " " + self.paterno + " " + self.materno
+		
