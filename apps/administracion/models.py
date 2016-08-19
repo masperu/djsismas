91af1cf5908966068efe4cedb30e56f20b81cc73
Carlos Yosimar Ruiz Vasquez <xcarlx2@gmail.com>
2016-08-19 16:56:42 -0500

    a

--- apps/administracion/models.py
+++ apps/administracion/models.py
@@ -2,21 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Organizacion(models.Model):
-    nombre = models.CharField(max_length=255)
-    ruc = models.CharField(max_length=11)
-    telefono = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(max_length=100, blank=True, null=True)
-    alcance = models.CharField(max_length=10)
-    siglas = models.CharField(max_length=10)
+	nombre = models.CharField(max_length=255)
+	ruc = models.CharField(max_length=11)
+	telefono = models.CharField(max_length=15, blank=True, null=True)
+	email = models.EmailField(max_length=100, blank=True, null=True)
+	alcance = models.CharField(max_length=10)
+	siglas = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.nombre
+	def __str__(self):
+		return self.nombre
 
 
 class TipoCalle(models.Model):
-    nombre = models.CharField(max_length=255)
-    codigo = models.CharField(max_length=10)
-    tipo = models.CharField(max_length=10)
-    
-    def __str__(self):
-        return self.nombre
+	nombre = models.CharField(max_length=255)
+	codigo = models.CharField(max_length=10)
+	tipo = models.CharField(max_length=10)
+	
+	def __str__(self):
+		return self.nombre
+
+
+class Ubigeo(models.Model):
+	codigo = models.CharField(max_length=8)
+	nombre = models.CharField(max_length=50)
--- apps/persona/models.py
+++ apps/persona/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from apps.administracion.models import *
 
 # Create your models here.
+
+
+class EstadoCivil(models.Model):
+    nombre = models.CharField(max_length=45)
+    codigo = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=45)
+    paterno = models.CharField(max_length=45)
+    materno = models.CharField(max_length=45)
+    correos = models.CharField(max_length=45)
+    dni = models.CharField(max_length=45)
+    clave = models.CharField(max_length=45)
+    sexo = models.CharField(max_length=45)
+    fnacimiento = models.CharField(max_length=45)
+    estadocivil = models.ForeignKey(EstadoCivil)
+    tipocalle = models.ForeignKey(TipoCalle)
+    direccion = models.CharField(max_length=600)
+    ubigeonacimiento = models.ForeignKey(Ubigeo)
+    ubigeoresidencia = models.ForeignKey(Ubigeo)
+    
+    def __str__(self):
+        return self.nombre
