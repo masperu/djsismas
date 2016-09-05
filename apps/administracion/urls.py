c871cfe90cff8fe96da370ea4f65651c7ab61a18
Carlos Yosimar Ruiz Vasquez <xcarlx2@gmail.com>
2016-09-05 08:09:59 -0500

    Login

--- apps/administracion/urls.py
+++ apps/administracion/urls.py
@@ -3,8 +3,8 @@ from apps.administracion.views import *
 
 urlpatterns = [
     url(r'^$', VistaInicio , name="home"),
+    # url(r'^home/$', VistaInicio , name="home"),
     url(r'^login/$', Login , name="login"),
-    url(r'^home/$', VistaHome , name="home"),
-    #url(r'^usuarios.usuario/listar/$', HomeView, name='user_list'),
+    url(r'^logout/$', Logout , name="logout"),
     #url(r'^usuarios.usuario/crear/$', UsuarioCrear, name='user_add'),
 ]
