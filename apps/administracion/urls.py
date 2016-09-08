641f80f104c3ce9a8db771bfb81a2a564f5b3922
Kenetk <KNTK@KENETK.local>
2016-09-08 11:53:51 -0500

    urls

--- apps/administracion/urls.py
+++ apps/administracion/urls.py
@@ -5,8 +5,46 @@ urlpatterns = [
     url(r'^$', VistaInicio , name="home"),
     url(r'^login/$', Login , name="login"),
     url(r'^logout/$', Logout , name="logout"),
+    
+    # MENUS
     url(r'^menu/$', ListaMenus , name="lista_menu"),
     url(r'^menu-form/$', MenuAgregar , name="menu-agregar"),
     url(r'^menu/eliminar/(?P<idmenu>[0-9]+)/$', MenuEliminar , name="menu-eliminar"),
+    # 
+	#
+	# 
+
+    # ROLES
+    # 
+    # 
+    # 
+    # 
+    #
+    # 
+
+    # ACCESOS
+    # 
+    # 
+    # 
+    # 
+    #
+    # 
+
+    # PERFIL
+    # 
+    # 
+    # 
+    # 
+    #
+    # 
+
+    # USUARIOS
+    # 
+    # 
+    # 
+    # 
+    #
+    # 
+
     #url(r'^usuarios.usuario/crear/$', UsuarioCrear, name='user_add'),
 ]
