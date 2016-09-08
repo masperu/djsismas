937b5605811336f9514e3c3a9dc14eaeca96c7ce
Carlos Yosimar Ruiz Vasquez <xcarlx2@gmail.com>
2016-09-08 15:15:30 -0500

    Menu

--- apps/administracion/forms.py
+++ apps/administracion/forms.py
@@ -8,7 +8,7 @@ class MenuForm(forms.ModelForm):
 	descripcion = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 	ruta = forms.CharField(max_length=350,widget=forms.TextInput(attrs={'class': 'form-control', 'rows':'2'}))
 	orden = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control', 'type':'number'}))
-	estado = forms.BooleanField(required=True,widget=forms.RadioSelect(choices=YES_OR_NO, attrs={'class': 'form-check-input'}))
+	estado = forms.BooleanField(required=True,widget=forms.RadioSelect(choices=YES_OR_NO))
 	menupadre = forms.ModelChoiceField(required=False,
 			queryset= Menu.objects.filter(estado=True), 
 			empty_label="(Menu Padre)",
