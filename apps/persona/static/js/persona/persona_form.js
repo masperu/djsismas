ad24610ba78f0a8d0da12dd073d1a830dd257a4c
Carlos Yosimar Ruiz Vasquez <xcarlx2@gmail.com>
2016-09-16 12:28:50 -0500

    modal

--- apps/persona/static/js/persona/persona_form.js
+++ apps/persona/static/js/persona/persona_form.js
@@ -6,4 +6,16 @@
 	$('#datetimepicker1').datetimepicker({
 		format:'YYYY-MM-DD',
 	});
+	// $("#idubigeonacimiento").click(function(e) {
+	// 	$('#modalUbigeo').modal('show').find('.modal-body').load($(this).attr('href'));
+	// 	// $('#modalUbigeo').on('shown.bs.modal', function() {
 
+	// 	// });
+
+
+	// });
+	$("btnUbigeoGuardar").on('click',function(e) {
+		alert("ASasas");
+		console.log(e);
+		// $('#modalUbigeo').modal('hide');
+	});
